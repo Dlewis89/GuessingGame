2c45cd93b7cb23969f729bbba4abb85186432d87
Demetrius Lewis <Demetriuslewis2012@gmail.com>
2024-08-14 09:24:04 -0400

    A quick guessing game while learning C#

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+Random random = new();
+
+int answer = random.Next(100);
+
+int guess = -1;
+
+Console.WriteLine("Guess a number between 0 and 100");
+
+
+while(answer != guess) {
+
+    try 
+    {
+        guess = Convert.ToInt32(Console.ReadLine());
+    } 
+    catch (Exception e) 
+    {
+        Console.WriteLine("Not the correct input type! Please enter a number next time");
+        break;
+    }
+
+    if (answer == guess) {
+        Console.WriteLine("You win!");
+        break;
+    }
+    else if(answer > guess) {
+        Console.WriteLine("a little higher");
+    }
+    else {
+        Console.WriteLine("a little lower");
+    }
+
+}
